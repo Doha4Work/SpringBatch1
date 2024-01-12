a996382a18ad68f77aea503724708bffbf8d504d
Doha4Work <doha4work@gmail.com>
2024-01-12 11:33:04 +0100

    Initializing spring batch project

---
+++ src/test/java/com/learndoha/springbatchproject1/SpringBatchProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learndoha.springbatchproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
